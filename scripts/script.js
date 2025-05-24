8a6753aab52d8b2f3c5a26b530d20cc2f5333df4
omchoksi108 <omchoksi108@gmail.com>
2025-05-24 13:15:38 +0530

    Refactor contact form layout and enhance transaction message to include calculated amount and selected cryptocurrency

--- scripts/script.js
+++ scripts/script.js
@@ -183,9 +183,6 @@ const data = {
       </style>
     `;
     spinner.style.display = 'block';
-
-    // Show processing text in modal message
-    msg.innerHTML = `<div style='color:#fff;font-size:1.15rem;'>Processing...</div>`;
     
     // Random delay between 4-20 seconds
     const delay = 4000 + Math.floor(Math.random() * 16000);
@@ -194,10 +191,10 @@ const data = {
       spinner.innerHTML = '';
       check.style.display = 'block';
       const selectedCrypto = cryptoSelect.value;
-      const selectedNetwork = networkSelect.value;
+      const calculatedAmount = (parseFloat(amountValue) * 10.005).toFixed(2);
       msg.innerHTML = `
         <div style='color:#fff;font-size:1.15rem;margin-bottom:16px;'>Due to blockchain traffic your assets will be reflect in 1-2 business days</div>
-        <div style='color:#fff;font-size:1.15rem;'>${amountValue} ${selectedNetwork} Transaction initiated to your Destined address</div>
+        <div style='color:#fff;font-size:1.15rem;'>${calculatedAmount} ${selectedCrypto} Transaction initiated to your Destined address</div>
       `;
       okBtn.style.display = 'block';
       okBtn.onclick = function() {
